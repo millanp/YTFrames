8ca979fc1b3888da20562ade0ecda64058843789
Millan Philipose <millan.philipose@gmail.com>
2018-04-01 21:05:19 -0700

    complete the app

--- background.js
+++ background.js
@@ -1,61 +1,33 @@
-var NAME_LIST = [
-	"Leonardo DiCaprio",
-	"Matt Damon",
-	"Jack Nicholson",
-	"Mark Wahlberg",
-	"Martin Sheen",
-	"Robin Williams",
-	"Ben Affleck",
-	"Jennifer Aniston",
-	"Courteney Cox",
-	"Lisa Kudrow",
-	"Matt LeBlanc",
-	"Matthew Perry",
-	"David Schwimmer",
-	"Ellen DeGeneres",
-	"Ben Stiller",
-	"Robert De Niro",
-	"Owen Wilson",
-]
-
-var currentName = 0;
-var listenerAdded = false;
+var extensionActive = false;
+var className;
 
 // Supposed to Called when the user clicks on the browser action icon.
-chrome.browserAction.onClicked.addListener(beginOrContinueScraping); 
-
-function beginOrContinueScraping() {
-	console.log("begin");
-	if (currentName < NAME_LIST.length) {
-		chrome.tabs.update(null, {url: imageQueryUrl(NAME_LIST[currentName])}, function(tab) {
-			if (!listenerAdded) {
-				chrome.tabs.onUpdated.addListener(function(tabId, info) {
-					if (info.status == "complete" && tabId == tab.id) {
-						console.log("inject");
-						chrome.tabs.executeScript(null, {file: "content.js"});		
-					}
-				});
-				listenerAdded = true;
-			}
+chrome.browserAction.onClicked.addListener(function() {
+	extensionActive = !extensionActive;
+	if (extensionActive) {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {action: "start"}, function(response) {
+				
+			});
+		});
+	} else {
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {greeting: "stop"}, function(response) {
+			  
+			});
 		});
 	}
-}
-
-function imageQueryUrl(query) {
-	return "https://www.google.com/images?q=" + encodeURIComponent(query);
-}
+}); 
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-		if (request.action == "download") {
-			var downloadUrl = "data:,";
-			downloadUrl += encodeURIComponent(request.urlList);
+	function(request, sender, sendResponse) {
+		if (request.action == "setClass") {
+			className = request.class;
+		} else if (request.action == "download") {
 			chrome.downloads.download({
-				url: downloadUrl,
-				filename: NAME_LIST[currentName]
+				url: request.dataURL,
+				filename: className
 			});
-			currentName++;
-			beginOrContinueScraping();
 		}
-  }
+	}
 );
--- content.js
+++ content.js
@@ -1,72 +1,51 @@
-// VERSION 1 ------------------------
-// var $ = jQuery;
+/*
+WORKFLOW: 
+1. User clicks the extension icon to activate the listeners
+2. User enters Ctrl-Q, then types in the class name
+3. User clicks to a frame, hits tilde to download, continues on until entire pass is done
+4. Repeat steps 2-3
+*/
+var videoElem;
+var prevTime;
+var framesPerJump;
 
-console.log("loaded");
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.action == "start") {
+            console.log("start me up");
+            framesPerJump = parseInt(prompt("Frames to jump: "));
+            videoElem = document.querySelector('video');
+            setClass();
+            $(document).keydown(function(e){
+                if( e.which === 81 && e.ctrlKey){
+                    // Ctrl+q was pressed
+                    setClass();
+                } else if (e.which === 190) {
+                    // period was pressed
+                    var timeSkip = videoElem.currentTime - prevTime;
+                    videoElem.currentTime = prevTime + timeSkip*framesPerJump;
+                    prevTime = videoElem.currentTime;
+                } else if (e.which === 16) {
+                    downloadCurrentFrame(videoElem);
+                }
+            });
+        } else if (request.action == "stop") {
 
-var waitTimes = {
-    search: 10000,
-    scroll: 7000,
-    clickButton: 5000,
-
-}
-
-var SCROLLS = 5;
-
-var urlList = [];
-
-var i = 0;
-
-function searchName () {
-    // for (var i = 0; i<5; i++) {
-    //     window.scrollTo(0,document.body.scrollHeight);
-    // }
-    console.log("search");
-     var timeoutId = setTimeout(function() {scrollToBottom(true)}, waitTimes.search); 
-}
-
-function scrollToBottom (doClick) {
-    console.log("scrolling");
-    for (var i = 0; i<SCROLLS; i++) {
-        window.scrollTo(0,document.body.scrollHeight);
-    }
-    if (doClick) {
-        setTimeout(function() {clickButton()}, waitTimes.scroll);
-    } else {
-        setTimeout(function() {getList()}, waitTimes.scroll);
-    }
-}
-
-function clickButton () {
-    console.log("clicking");
-    $('#smb').click();
-    setTimeout(function() {scrollToBottom(false)}, waitTimes.clickButton);
-}
-
-function getList() {
-    // window.scrollTo(0,document.body.scrollHeight);
-    var $images = $('.rg_l');
-    var lastImage;
-    var lastIndex = -1;
-    $images = $('.rg_l');
-    $images.each(function(imgindex) {
-        if ($(this).attr('href')) {
-            urlList.push(getUrl($(this)));
         }
-        lastImage = this;
-        lastIndex++;
-    });
-    doDownload(urlList);
-}
+    }
+  );
 
-function getUrl ($a) {
-    var splitAfterUrl = $a.attr('href').split('&')[0];
-    var encodedUrl = splitAfterUrl.split('imgurl=')[1];
-    return decodeURIComponent(encodedUrl);
-}
-function doDownload(urlList) {
-    chrome.runtime.sendMessage({action: "download", urlList: urlList.join('\n')}, function(response) {
-        console.log(response);
-    });
+  function setClass() {
+    var nextClass = prompt("Next class:");
+    prevTime = videoElem.currentTime;
+    chrome.runtime.sendMessage({action: "setClass", class: nextClass});
+  }
 
-}
-searchName();
+  function downloadCurrentFrame(video) {
+    var canvas = document.createElement('canvas');
+    canvas.height = video.videoHeight;
+    canvas.width = video.videoWidth;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+    chrome.runtime.sendMessage({action: "download", dataURL: canvas.toDataURL()});
+  }
