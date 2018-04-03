2c6ac43ca9d304101a8d173e777ba88d20ba0949
millanp <millan.philipose@gmail.com>
2018-04-02 18:51:02 -0700

    get comma button to work and add Ctrl-Shift-F to change frame jump

--- content.js
+++ content.js
@@ -13,39 +13,46 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action == "start") {
             console.log("start me up");
-            framesPerJump = parseInt(prompt("Frames to jump: "));
+            setFramesPerJump()
             videoElem = document.querySelector('video');
             setClass();
-            $(document).keydown(function(e){
-                if( e.which === 81 && e.ctrlKey){
+            $(document).keydown(function(e) {
+                if (e.which === 81 && e.ctrlKey) {
                     // Ctrl+q was pressed
                     setClass();
-                } else if (e.which === 190) {
-                    // period was pressed
+                } else if (e.which === 190 || e.which === 188) {
+                    // period or comma as pressed
                     var timeSkip = videoElem.currentTime - prevTime;
-                    videoElem.currentTime = prevTime + timeSkip*framesPerJump;
+                    videoElem.currentTime = prevTime + timeSkip * framesPerJump;
                     prevTime = videoElem.currentTime;
                 } else if (e.which === 16) {
                     downloadCurrentFrame(videoElem);
+                } else if (e.which === 70 && e.ctrlKey && e.altKey) {
+                    // Ctrl+alt+f was pressed
+                    setFramesPerJump();
                 }
             });
         } else if (request.action == "stop") {
 
         }
     }
-  );
+);
 
-  function setClass() {
+function setFramesPerJump() {
+    framesPerJump = parseInt(prompt("Frames to jump: "));
+}
+
+function setClass() {
     var nextClass = prompt("Next class:");
     prevTime = videoElem.currentTime;
-    chrome.runtime.sendMessage({action: "setClass", class: nextClass});
-  }
+    chrome.runtime.sendMessage({ action: "setClass", class: nextClass });
+}
 
-  function downloadCurrentFrame(video) {
+function downloadCurrentFrame(video) {
     var canvas = document.createElement('canvas');
     canvas.height = video.videoHeight;
     canvas.width = video.videoWidth;
     var ctx = canvas.getContext('2d');
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    chrome.runtime.sendMessage({action: "download", dataURL: canvas.toDataURL()});
-  }
+    chrome.runtime.sendMessage({ action: "download", dataURL: canvas.toDataURL() });
+}
